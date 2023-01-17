7a9fb96fc64bd6e5aec6402ad6d5f1ce52f70616
mcquo011 <omcquown@gmail.com>
2023-01-17 13:20:55 -0600

    added to weather function

--- assets/script.js
+++ assets/script.js
@@ -1,11 +1,13 @@
 let search = document.querySelector('#button');
 let city = document.querySelector('#inputValue');
 let currentDay = document.querySelector('#currentDay');
-let currentCity = document.querySelector('#currentCity')
+let cityHistory = document.querySelector('.searchHistory')
 let temp = document.querySelector('#temp');
 let humidity = document.querySelector('#humidity');
 let wind = document.querySelector('#wind');
-
+// history var
+let history = [];
+let forecast = document.querySelector('.forecast')
 
 let APIkey = "3118a6b9d3ec49beecd43997664c59b9";
   
@@ -20,25 +22,66 @@ function weather(cityName) {
      fetch(queryURL)
        .then((response) => response.json())
        .then((data) => {
-        console.log(data)
-        let date = new Date();
-        let currentDate = date.toLocaleDateString();
-        currentCity.innerHTML = data.name
-        currentDay.innerHTML = data.name + " ("+ currentDate +")"
-        let fahrenheitTemp = Math.round(
-          ((data.main.temp - 273.15) * 9) / 5 + 32
-        );
-        temp.textContent = "Temperature: " + fahrenheitTemp + "°F"
-        humidity.innerHTML = "Humidity: " + data.main.humidity + "%"
-        wind.innerHTML = "wind speed: " + data.wind.speed + "mph"
+         console.log(data);
+         let date = new Date();
+         let currentDate = date.toLocaleDateString();  
+         currentDay.innerHTML = data.name + " (" + currentDate + ")";
+         // convert to fahrenheit
+         let fahrenheitTemp = Math.round(
+           ((data.main.temp - 273.15) * 9) / 5 + 32
+         );
+         temp.innerHTML = "Temperature: " + fahrenheitTemp + "°F";
+         humidity.innerHTML = "Humidity: " + data.main.humidity + " %";
+         wind.innerHTML = "wind speed: " + data.wind.speed + " m/s";
 
-       }
-       )
+         cityHistory.innerHTML = data.name;
+          // Add the searched city to an array
+          history.push(data.name);
+          // Save the array to local storage
+          localStorage.setItem("history", JSON.stringify(history));
+          console.log(history);
+       })
        .catch((error) => console.log(error));
+       
+       
+       // how do I use lon and lat ??? do I need to?? 
+       let forecastQueryURL =
+      "https://api.openweathermap.org/data/2.5/forecast?q=" + cityName +
+     "&appid=" + APIkey;
+
+  fetch(forecastQueryURL)
+    .then((response) => response.json())
+    .then((data) => {
+      const fiveDayForecast = data.list.slice(0, 5);
+      for (let i = 0; i < fiveDayForecast.length; i++) {
+        let forecast = fiveDayForecast[i];
+        console.log(
+          forecast.dt_txt +
+            ": " +
+            forecast.main.temp +
+            "F, " + forecast.wind.speed + " m/s " +
+            forecast.weather[0].description
+        );
+      }
+    })
+    .catch((error) => console.log(error));
 }
 
-search.addEventListener('click', function(){
+
+// event listener when search button is clicked
+search.addEventListener("click", function (event) {
   event.preventDefault();
-  let searchCity = city.value
-  weather(searchCity);
-}) 
+  let cityName = city.value;
+  weather(cityName);
+  // only show when search button is clicked and a city is entered 
+  document.querySelectorAll(".d-none").forEach(function (element) {
+    element.classList.toggle("d-none");
+
+  });
+});
+
+// save search history to local storage
+// add an event listener to the history buttons 
+
+
+
